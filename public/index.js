2e2271e3de01b091c5c69f3acad8a3a47f96334d
330namajnoon <92964328+330namajnoon@users.noreply.github.com>
2023-02-23 11:45:55 +0100

    Add files via upload

--- public/index.js
+++ public/index.js
@@ -39,6 +39,7 @@ function Neshan() {
         if(game.jugar) {
         this.d = false;
         this.tira(e);
+        game.jugar = false;
         }
     })
 }
@@ -85,6 +86,12 @@ function Pelota(x=0,y=0,r=10,n= 1,sx=0,sy=0) {
 Pelota.prototype.draw = function() {
     let {x,y,r,n} = this.data;
     canvas.ctx.fillStyle = "#F55050";
+    canvas.ctx.strokeStyle = "#00000000";
+    if(game.frame % 10 == 0 ) {
+        canvas.ctx.beginPath();
+        canvas.ctx.arc(porsentage(x), porsentage(y), porsentage(r), 0, 2 * Math.PI);
+        canvas.ctx.stroke();
+    }
     canvas.ctx.beginPath();
     canvas.ctx.arc(porsentage(x), porsentage(y), porsentage(r), 0, 2 * Math.PI);
     canvas.ctx.fill();
@@ -93,6 +100,7 @@ Pelota.prototype.draw = function() {
     canvas.ctx.font = `${porsentage(r)/2}px Arial`;
     canvas.ctx.fillText("GMV", porsentage(x)-(porsentage(r)/1.6), porsentage(y)+(porsentage(r)/4));
     if(this.t) {
+        canvas.ctx.strokeStyle = "#ffffff";
         canvas.ctx.font = porsentage(20)+"px Arial";
         canvas.ctx.strokeText(`${this.data.n}`, porsentage(45), porsentage(35));
     }
@@ -124,12 +132,39 @@ function Pared(x=0,y=0,w=100,h=100,l = "l",colors={},permiso) {
     this.data = {x,y,w,h,l,colors,permiso};
 }
 Pared.prototype.draw = function() {
-    let {x,y,w,h,permiso} = this.data;
+    let {x,y,w,h,permiso,l} = this.data;
     if(permiso) {
-        canvas.ctx.fillStyle = this.data.colors.c1;
-        canvas.ctx.fillRect(porsentage(x),porsentage(y),porsentage(w),porsentage(h));
-        canvas.ctx.strokeStyle = this.data.colors.c2;
-        canvas.ctx.strokeRect(porsentage(x),porsentage(y),porsentage(w),porsentage(h));
+        if(l !== "f") {
+            canvas.ctx.fillStyle = this.data.colors.c1;
+            canvas.ctx.fillRect(porsentage(x),porsentage(y),porsentage(w),porsentage(h));
+            canvas.ctx.strokeStyle = this.data.colors.c2;
+            canvas.ctx.lineWidth = 2;
+        }
+        switch (l) {
+            case "l":
+                canvas.ctx.moveTo(porsentage(x)+porsentage(w),porsentage(y));
+                canvas.ctx.lineTo(porsentage(x)+porsentage(w),porsentage(y)+porsentage(h));
+                canvas.ctx.stroke();
+                break;
+                case "r":
+                    canvas.ctx.moveTo(porsentage(x),porsentage(y));
+                    canvas.ctx.lineTo(porsentage(x),porsentage(y)+porsentage(h));
+                    canvas.ctx.stroke();
+                    break; 
+                    case "t":
+                        canvas.ctx.moveTo(porsentage(x),porsentage(y)+porsentage(h));
+                        canvas.ctx.lineTo(porsentage(x)+porsentage(w),porsentage(y)+porsentage(h));
+                        canvas.ctx.stroke();
+                        break;
+                        case "b":
+                            canvas.ctx.moveTo(porsentage(x),porsentage(y));
+                            canvas.ctx.lineTo(porsentage(x)+porsentage(w),porsentage(y));
+                            canvas.ctx.stroke();
+                            break;            
+                    
+        
+           
+        }
     }
 }
 Pared.prototype.update = function() {
@@ -157,7 +192,7 @@ Pared.prototype.update = function() {
                 game.pelota.data.sy = game.pelota.data.sy*-1;
                 break;    
             case "f":
-                if(game.pelota.data.n < 1) {
+                if(game.pelota.data.n <= 0) {
 
                     game.ganar();
                 }else {
@@ -170,7 +205,8 @@ Pared.prototype.update = function() {
 
 
 function Game(niveles,nivel) {
-    this.jugar = true;
+    this.frame = 0;
+    this.jugar = false;
     this.numNivel = nivel;
     this.niveles = niveles;
     this.neshan = new Neshan();
@@ -197,8 +233,9 @@ function Game(niveles,nivel) {
                     this.numNivel = 1;
                     game.crearNivel();
                     setTimeout(()=> {
-                    this.jugar = true;
-                    },100)
+                        this.jugar = true;
+                    },1000)
+                    return 1
                 }
                 if(this.numNivel < this.niveles.length) {
                     this.g = false;
@@ -206,16 +243,16 @@ function Game(niveles,nivel) {
                     game.crearNivel();
                     setTimeout(()=> {
                     this.jugar = true;
-                    },100)
+                    },1000)
                 }
             }
             if(this.f) {
+                this.jugar = false;
                 this.f = false;
                 game.crearNivel();
                 setTimeout(()=> {
                     this.jugar = true;
-                },100)
-
+                },1000)
             }
         }
         
@@ -229,6 +266,12 @@ function Game(niveles,nivel) {
 
 
 Game.prototype.draw = function() {
+    this.paredes.forEach(p => {
+        p.draw();
+    })
+    this.neshan.draw();
+    this.pelota.draw();
+    canvas.ctx.strokeStyle = "#ffffff";
     if(this.g) {
         canvas.ctx.font = porsentage(10)+"px Arial";
         canvas.ctx.strokeText(`Has ganado!!!`, porsentage(20), porsentage(50));
@@ -245,7 +288,7 @@ Game.prototype.draw = function() {
     }
     if(this.f) {
         canvas.ctx.font = porsentage(10)+"px Arial";
-        canvas.ctx.strokeText(`Has fayado!!!`, porsentage(20), porsentage(50));
+        canvas.ctx.strokeText(`Has fallado!!!`, porsentage(20), porsentage(50));
         let {x,y,w,h} = this.button;
         canvas.ctx.fillStyle = "#F48484";
         canvas.ctx.fillRect(porsentage(x),porsentage(y),porsentage(w),porsentage(h));
@@ -254,11 +297,6 @@ Game.prototype.draw = function() {
         canvas.ctx.fillText("Otra vez",porsentage(x+h),porsentage(y+(h/1.5)));
        
     }
-    this.paredes.forEach(p => {
-        p.draw();
-    })
-    this.neshan.draw();
-    this.pelota.draw();
 
 }
 Game.prototype.update = function() {
@@ -282,14 +320,14 @@ Game.prototype.fayar = function() {
 }
 Game.prototype.crearNivel = function() {
     
-    
+    this.paredes = [];
     this.niveles[this.numNivel-1].paredes.forEach((p,index) => {
         let permiso;
-        let colors = {c1:"#E8D2A6",c2:"#86A3B8"};
+        let colors = {c1:"#E8D2A6",c2:"#F16767"};
         if(index < this.niveles[this.numNivel-1].paredes.length - 2){
             permiso = true;
         }else {
-            permiso = false;
+            permiso = true;
         } 
         this.paredes.push(new Pared(p[0],p[1],p[2],p[3],p[4],colors,permiso));
     })
@@ -310,11 +348,20 @@ Game.prototype.crearNivel = function() {
 function anim() {
     requestAnimationFrame(anim);
     canvas.ctx.clearRect(0,0,porsentage(100),porsentage(100));
+    game.frame++;
     game.draw();
     game.update();
 }
 
+function play(niveles,numNivel) {
 
+    game = new Game(niveles,numNivel);
+    game.crearNivel();
+    setTimeout(()=> {
+        game.jugar = true;
+    },200)
+
+}
 
 
 function descargarNiveles() {
@@ -322,9 +369,7 @@ function descargarNiveles() {
     http.open("POST","/descargar_niveles",true);
     http.onreadystatechange = function() {
         if(http.status == 200 && http.readyState == 4) {
-            game = new Game(JSON.parse(http.responseText),1);
-           
-            game.crearNivel();
+            play(JSON.parse(http.responseText),4)
             anim();
            
         }
